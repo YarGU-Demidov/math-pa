5ffaf951c61166d73e48bf4cbb2b3916180dbf58
Andrey Mokeev <andrey@mokeev1995.ru>
2017-04-09 03:55:52 +0300

    more usefull code

--- src/app/routes/users-controller/users-controller.component.ts
+++ src/app/routes/users-controller/users-controller.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import { Dialog, LazyLoadEvent } from 'primeng/primeng';
 import { SortDirection } from '../../../core/enums/sort-direction';
 import {
-	FiltersAndSortData, FiltersData,
+	FiltersAndSortData,
+	FiltersData,
 	UsersSortable
 } from '../../../core/services/api-service/controllers/users-info-controller';
 import { LoadingIndicatorService } from '../../../core/services/loading-indicator/loading-indicator.service';
@@ -20,58 +21,74 @@ import { UsersProviderWithLoader } from './users-provider-with-loader';
 	encapsulation: ViewEncapsulation.None
 })
 export class UsersControllerComponent implements OnInit {
-	
+
 	public users: UserInfo[] = [];
 	public usersCount: number = 0;
-	
+
 	public itemsPerPageCount: number = 100;
 	public createAreaShowed = false;
-	
+
 	@ViewChild('createDialog')
 	private createDialog: Dialog;
-	
+
 	private indicator: LoadingIndicatorService;
 	public usersProvider: UsersProvider;
-	
+
 	public constructor(usersProvider: UsersProviderWithLoader, indicator: LoadingIndicatorService) {
 		this.usersProvider = usersProvider;
 		this.indicator = indicator;
 	}
-	
+
 	public ngOnInit(): void {
 		this.usersProvider.getCount().then((count) => {
 			this.usersCount = count;
 		});
 	}
-	
+
 	public onCreateUser() {
 		this.createAreaShowed = true;
 	}
-	
+
 	public handleChange($event: { originalEvent: MouseEvent, index: number }): void {
 		this.createDialog.center();
 	}
-	
+
 	public loadUsersLazy($event: LazyLoadEvent): void {
-		const sortData: UsersSortable = {
-			group: SortDirection.Default
-		};
-		
-		const filterData: FiltersData = {
-			globalFilter: null
-		};
-		
-		const filtersAndSortData: FiltersAndSortData = {
-			sort: sortData,
-			filter: filterData
-		}
-		
-		if ($event.sortField === 'user.group') {
-			sortData.group = $event.sortOrder < 0 ? SortDirection.Descending : SortDirection.Ascending;
-		}
-		
+		const filtersAndSortData = this.buildData($event);
+
 		this.usersProvider.getItems($event.first, $event.rows, filtersAndSortData).then((users: UserInfo[]) => {
 			this.users = users;
 		});
+	}
+
+	private buildData(event: LazyLoadEvent): FiltersAndSortData {
+
+		const sortData = this.buildSortData(event),
+			filterData = this.buildFilterData(event);
+
+		return {
+			sort  : sortData,
+			filter: filterData
+		};
+	}
+
+	private buildSortData(event: LazyLoadEvent): UsersSortable {
+		const sortData: UsersSortable = {
+			group: SortDirection.Default
+		};
+
+		if (event.sortField === 'user.group') {
+			sortData.group = event.sortOrder < 0 ? SortDirection.Descending : SortDirection.Ascending;
+		}
+
+		return sortData;
+	}
+
+	private buildFilterData(event: LazyLoadEvent): FiltersData {
+		const filterData: FiltersData = {
+			globalFilter: event.globalFilter || null
+		};
+
+		return filterData;
 	}
 }
